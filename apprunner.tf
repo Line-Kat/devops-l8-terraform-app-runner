aab9403010dc3eb6006850592ba02cdd8f24e38a
Line-Kat <l_katralen@hotmail.com>
2024-11-07 13:55:44 +0000

    workflow

--- apprunner.tf
+++ apprunner.tf
@@ -1,11 +1,9 @@
 resource "aws_apprunner_service" "service" {
   service_name = var.prefix
   source_configuration {
-
     authentication_configuration {
       access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
     }
-
     image_repository {
       image_configuration {
         port = "8080"
